7015dbe0db2a1e5fe4384250dfeb9a4f5390dc56
1Glizda <bodo.bogdan2020@gmail.com>
2025-02-20 00:29:37 +0200

    Game CI integration

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -1,25 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using TMPro;
-using Unity.VisualScripting;
-
 
 public class HumanController : MonoBehaviour
 {
     public static HumanController instance;
     public float speed;
-    Rigidbody2D playerRigidbody;
-    BoxCollider2D playerCollider;
-    public static int collectedAmount = 0;
+    private Rigidbody2D playerRigidbody;
+    private BoxCollider2D playerCollider;
     private SpriteRenderer spriteRenderer;
     public Animator animator;
     public RuntimeAnimatorController defaultController;
+    public static int collectedAmount = 0;
     public static int roomState = 1;
     private GameObject minimapCanvas;
-    
+    private Vector2 movement;
+    private Vector2 lastDirection = new Vector2(0, -1); // Default facing front
 
     void Start()
     {
@@ -27,7 +21,6 @@ public class HumanController : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody2D>();
         playerCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
         animator.runtimeAnimatorController = defaultController;
         minimapCanvas = GameObject.Find("MiniMap Canvas");
     }
@@ -44,44 +37,34 @@ public class HumanController : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) vertical = 1;
 
         Vector2 targetVelocity = new Vector2(horizontal * speed, vertical * speed);
-        playerRigidbody.velocity = Vector2.Lerp(playerRigidbody.velocity, targetVelocity, Time.deltaTime * smoothFactor);    
+        playerRigidbody.velocity = Vector2.Lerp(playerRigidbody.velocity, targetVelocity, Time.deltaTime * smoothFactor);
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
+        // Get movement input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        //  if(roomState == 1) {
-        //     if (horizontal >= 0f)
-        //     {
-        //         transform.localScale = new Vector2(0.12f, 0.12f);
-        //     }
-        //     else if (horizontal < 0f)
-        //     {
-        //         transform.localScale = new Vector2(-0.12f, 0.12f);
-        //     }
-        // }
+        // Prevent diagonal flipping issue
+        if (movement != Vector2.zero)
+        {
+            // Prioritize horizontal movement first
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                lastDirection = new Vector2(movement.x, 0);
+            }
+            else
+            {
+                lastDirection = new Vector2(0, movement.y);
+            }
+        }
 
-        if (playerRigidbody.velocity.magnitude > 0.1f)
-        {
-            animator.Play("Move");
-        }
-        else
-        {
-            animator.Play("Idle");
-        }
+        // Pass values to Animator
+        animator.SetFloat("MoveX", lastDirection.x);
+        animator.SetFloat("MoveY", lastDirection.y);
+
+        // Use Animator parameter instead of forcing animation
+        animator.SetBool("isMoving", playerRigidbody.velocity.magnitude > 0.1f);
     }
-  
-
-    private void OnTriggerStay2D(Collider2D other)
-    {}
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {}
-
-    private void OnTriggerExit2D(Collider2D other)
-    {}
 }
-
-    //fixed update 50 PFS (1/50 -> 0.02) 0.02 * 100 -> 2m
-    //update 10 FPS (dt = 1 / 10 -> 0.1) 0.1 * 100 -> 10 m
