2e9f2f41f5cb18b69cacb4680e0993514880113f
1Glizda <bodo.bogdan2020@gmail.com>
2025-02-20 02:24:40 +0200

    new build config 2

---
+++ Assets/Candle3_Trigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candle3_Trigger : MonoBehaviour
+{
+    void OnTriggerEnter2D() {
+        if(Input.GetKey(KeyCode.RightShift)) {
+        }
+    }
+
+    void OnTriggerStay2D() {
+        if(Input.GetKey(KeyCode.RightShift)) {
+        }
+    }
+}
