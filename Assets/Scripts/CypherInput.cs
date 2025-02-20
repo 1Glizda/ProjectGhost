bfe63ff9730d2504ab291edabbe833b7eadf2f78
1Glizda <bodo.bogdan2020@gmail.com>
2025-02-20 05:31:23 +0200

    Rooms 1-5 full

---
+++ Assets/Scripts/CypherInput.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+
+public class CypherInput : MonoBehaviour
+{
+    public TMP_InputField inputField; // Assign in Inspector
+    public int correctCode = 1234; // Set the correct number
+    public Door door5;
+
+    void Update()
+    {
+        // Check if Enter key is pressed
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ValidateCode();
+            return;
+        }
+    }
+
+    public void ValidateCode()
+    {
+        if (int.TryParse(inputField.text, out int enteredCode))
+        {
+            if (enteredCode == correctCode)
+            {
+                Debug.Log("Correct Code!");
+                door5.PassDoor();
+            }
+            else
+            {
+                Debug.Log("Wrong Code!");
+                inputField.text = ""; // Clear field for retry
+            }
+        }
+        else
+        {
+            Debug.Log("Invalid Input! Enter a number.");
+            inputField.text = ""; // Reset invalid input
+        }
+    }
+}
---
+++ Assets/Scripts/Triggers/CypherTrigger.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class CypherTrigger : MonoBehaviour
+{
+    public GameObject cypherUI; // Assign in the Inspector
+
+    void Start()
+    {
+        // If not assigned in the Inspector, try finding the Cypher GameObject
+        if (cypherUI == null)
+        {
+            cypherUI = GameObject.Find("Cypher");
+        }
+
+        // Make sure it's initially disabled
+        if (cypherUI != null)
+        {
+            cypherUI.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            ActivateCypher();
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            ActivateCypher();
+        }
+    }
+
+    void ActivateCypher()
+    {
+        if (cypherUI != null)
+        {
+            cypherUI.SetActive(true);
+            Debug.Log("Cypher activated!");
+        }
+        else
+        {
+            Debug.LogWarning("Cypher GameObject not found!");
+        }
+    }
+}
